7c2bd3e9f5dbda986a843ade9a3038d3031a6a9e
EmelyanovAlexey <emelyanov_98@mail.ru>
2023-10-06 11:56:00 +0700

    222

---
+++ 3/ex2/src/action_tutorials_interfaces/src/fibonacci_action_server.py
@@ -0,0 +1,54 @@
+import time
+
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from action_tutorials_interfaces.action import Fibonacci
+
+
+class FibonacciActionServer(Node):
+
+    def __init__(self):
+        super().__init__('fibonacci_action_server')
+        self._action_server = ActionServer(
+            self,
+            Fibonacci,
+            'fibonacci',
+            self.execute_callback)
+    # Узел ROS 2 для добавления клиента действий: self.
+    # Тип действия: Fibonacci(импортируется в строке 5).
+    # Название действия: 'fibonacci'.
+    # Функция обратного вызова для выполнения принятых целей: self.execute_callback. Этот обратный вызов должен возвращать сообщение о результате для типа действия.
+
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+
+        feedback_msg = Fibonacci.Feedback()
+        feedback_msg.partial_sequence = [0, 1]
+
+        for i in range(1, goal_handle.request.order):
+            feedback_msg.partial_sequence.append(
+                feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i-1])
+            self.get_logger().info('Feedback: {0}'.format(feedback_msg.partial_sequence))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
+
+        goal_handle.succeed() # указать, что цель была успешной:
+
+        result = Fibonacci.Result()
+        result.sequence = feedback_msg.partial_sequence
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    fibonacci_action_server = FibonacciActionServer()
+
+    rclpy.spin(fibonacci_action_server)
+
+
+if __name__ == '__main__':
+    main()
