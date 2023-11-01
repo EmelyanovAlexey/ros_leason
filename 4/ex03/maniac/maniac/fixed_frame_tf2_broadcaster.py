dfacee1b6a5808b8ceb6cf58969fc605b9e56fee
EmelyanovAlexey <emelyanov_98@mail.ru>
2023-11-01 23:51:05 +0700

    5й

---
+++ 4/ex03/maniac/maniac/fixed_frame_tf2_broadcaster.py
@@ -0,0 +1,47 @@
+# Код очень похож на пример урока tf2 Broadcaster, 
+# единственное отличие состоит в том, что преобразование здесь не меняется со временем.
+
+from geometry_msgs.msg import TransformStamped
+
+import rclpy
+from rclpy.node import Node
+
+from tf2_ros import TransformBroadcaster
+
+
+class FixedFrameBroadcaster(Node):
+
+    def __init__(self):
+        super().__init__('fixed_frame_tf2_broadcaster')
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.timer = self.create_timer(0.1, self.broadcast_timer_callback)
+
+    def broadcast_timer_callback(self):
+        # Здесь мы создаем новое преобразование от родителя turtle1к 
+        # новому дочернему элементу carrot1. 
+        # Рамка carrot1смещена на 2 метра по оси Y относительно рамки 
+        t = TransformStamped()
+
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'turtle1'
+        t.child_frame_id = 'carrot1'
+        t.transform.translation.x = 0.0
+        t.transform.translation.y = 2.0
+        t.transform.translation.z = 0.0
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
+
+        self.tf_broadcaster.sendTransform(t)
+
+
+def main():
+    rclpy.init()
+    node = FixedFrameBroadcaster()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
